d7a6d706d5801680c7d387ffdc3a80fa3709ef28
Krina Wibisana <krinawibisana@gmail.com>
2020-07-19 08:20:18 +0700

    master: update styles

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { IWeatherComponent } from "../../interfaces";
+import { Row, Col, Typography } from "antd";
+const { Title, Text } = Typography;
 
 const Weather = (props: IWeatherComponent) => {
   return (
-    <div className="city">
-      <h2 className="city-name">
+    <Row className="city">
+      <Title level={2} className="city-name">
         <span>{props.name}</span>
         <sup>{props.country}</sup>
-      </h2>
-      <div className="city-temp">
+      </Title>
+      <Col className="city-temp">
         {Math.round(props.temp)}
         <sup>&deg;C</sup>
-      </div>
-      <div className="info">
+      </Col>
+      <Col className="info">
         <img
           className="city-icon"
           src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
           alt={props.description}
         />
-        <p>{props.description}</p>
-      </div>
-    </div>
+        <Text className="city-description">{props.description}</Text>
+      </Col>
+    </Row>
   );
 };
 
